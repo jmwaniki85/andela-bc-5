a5494656f5b4e65818ac50730d0d0d3e206042a3
Denis Korir <dennokorir@gmail.com>
2016-01-26 15:04:57 +0300

    New reverse function

--- reverse_string.py
+++ reverse_string.py
@@ -6,3 +6,15 @@ print(reverse_string("hello"))
 print(reverse_string("Andela"))
 print(reverse_string("Andela Bootcamp test"))
 #comment
+
+ef reverse_x(s):
+    s = list(s)
+    length = len(s)
+    for i in range(length//2):
+        swap(s,i,length-1-i)
+                
+    return s
+print (reverse_x("Denis"))
+
+
+
