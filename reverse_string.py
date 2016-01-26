f1716d15d158cb4686a099961cd3aec189fadcc7
Denis Korir <Denis Korir>
2016-01-26 12:55:57 +0300

    Python reverse function

---
+++ reverse_string.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    return(s[::-1])
+
+'''some working Examples'''
+print(reverse_string("hello"))
+print(reverse_string("Andela"))
+print(reverse_string("Andela Bootcamp test"))
